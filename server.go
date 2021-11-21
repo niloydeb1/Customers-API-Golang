096df0fb9a989f5ef324e024d9a587c119d857d2
Niloy Deb Roy <=>
2021-11-22 02:33:31 +0600

    Customer Api with mongoDB integrated.

--- server.go
+++ server.go
@@ -1,65 +1,115 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
-func getAllCustomer(context echo.Context) error {
-	if len(customersDB) == 0 {
+
+func (customers *Customers) getAllCustomer(context echo.Context) error {
+	client, ctx, customerCollection := getDB()
+	defer client.Disconnect(ctx)
+
+	cursor, err := customerCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+
+	if err = cursor.All(ctx, &customers.customers); err != nil {
+		log.Fatal(err)
+	}
+	if len(customers.customers) == 0 {
 		return context.JSON(http.StatusNotFound, "No customers available!")
 	}
-	return context.JSON(http.StatusOK, customersDB)
+	return context.JSON(http.StatusOK, customers.customers)
 }
 
-func getCustomer(context echo.Context) error {
+func (customer *Customer) getCustomer(context echo.Context) error {
 	id := context.Param("id")
-	pop, ok := customersDB[id]
-	if ok {
-		return context.JSON(http.StatusOK, pop)
+
+	client, ctx, customerCollection := getDB()
+	defer client.Disconnect(ctx)
+
+	if err := customerCollection.FindOne(ctx, bson.M{"id": id}).Decode(&customer); err != nil {
+		return context.JSON(http.StatusNotFound, "Customer not found!")
 	}
-	return context.JSON(http.StatusNotFound, "Customer not found!")
+	return context.JSON(http.StatusOK, customer)
 }
 
-func updateCustomer(context echo.Context) error {
+func (customer *Customer) updateCustomer(context echo.Context) error {
 	type body struct {
-		Name string			`json:"Name" validate:"required"`
-		Address string		`json:"Address" validate:"required"`
+		Name    string `json:"Name" validate:"required"`
+		Address string `json:"Address" validate:"required"`
 	}
 	var reqBody body
 	if err := context.Bind(&reqBody); err != nil {
 		return err
 	}
-
 	if err := validator.New().Struct(reqBody); err != nil {
 		return context.JSON(http.StatusNotFound, "invalid format!")
 	}
 
 	id := context.Param("id")
-	_, ok := customersDB[id]
-	if ok {
-		customersDB[id] = Customer{
-			Id: id,
-			Name: reqBody.Name,
-			Address: reqBody.Address,
-		}
-		return context.JSON(http.StatusOK, customersDB[id])
+	client, ctx, customerCollection := getDB()
+	defer client.Disconnect(ctx)
+
+	filter := bson.M{"id": id}
+	update := bson.D{
+		{"$set", bson.D{
+			{"name", reqBody.Name},
+			{"address", reqBody.Address},
+		},
+		},
 	}
-	return context.JSON(http.StatusNotFound, "Customer not found!")
+	result, err := customerCollection.UpdateOne(
+		ctx,
+		filter,
+		update,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if result.MatchedCount == 0 {
+		return context.JSON(http.StatusNotFound, "Customer not found!")
+	}
+	fmt.Printf("Updated %v Document!\n", result.ModifiedCount)
+	return context.JSON(http.StatusOK, "Customer updated successfully")
 }
 
-func deleteCustomer(context echo.Context) error {
+func (customer *Customer) deleteCustomer(context echo.Context) error {
 	id := context.Param("id")
-	pop, ok := customersDB[id]
-	if ok {
-		delete(customersDB, id)
-		return context.JSON(http.StatusOK, fmt.Sprintf("%v Deleted!", pop.Name))
+
+	client, ctx, customerCollection := getDB()
+	defer client.Disconnect(ctx)
+
+	filter := bson.M{"id": id}
+	result, err := customerCollection.DeleteOne(
+		ctx,
+		filter,
+	)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return context.JSON(http.StatusNotFound, "Customer Not Found!")
+
+	if result.DeletedCount == 0 {
+		return context.JSON(http.StatusNotFound, "Customer Not Found!")
+	}
+	fmt.Printf("Deleted %v Document!\n", result.DeletedCount)
+	return context.JSON(http.StatusOK, "Customer Deleted successfully")
 }
 
 func validateId(next echo.HandlerFunc) echo.HandlerFunc {
@@ -71,10 +121,10 @@ func validateId(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 
-func addCustomer(context echo.Context) error {
+func (customer Customer) addCustomer(context echo.Context) error {
 	type body struct {
-		Name string			`json:"Name" validate:"required"`
-		Address string		`json:"Address" validate:"required"`
+		Name    string `json:"Name" validate:"required"`
+		Address string `json:"Address" validate:"required"`
 	}
 	var reqBody body
 	if err := context.Bind(&reqBody); err != nil {
@@ -85,43 +135,72 @@ func addCustomer(context echo.Context) error {
 		return context.JSON(http.StatusNotFound, "invalid format!")
 	}
 
-	id := strconv.Itoa(idCounter)
-	idCounter++
-	customersDB[id] = Customer{
-		Id: id,
-		Name: reqBody.Name,
+	client, ctx, customerCollection := getDB()
+	defer client.Disconnect(ctx)
+
+	cursor, err := customerCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(ctx)
+
+	max := 0
+	for cursor.Next(ctx) {
+		if err = cursor.Decode(&customer); err != nil {
+			log.Fatal(err)
+		}
+		if id, _ := strconv.Atoi(customer.Id); id > max {
+			max = id
+		}
+	}
+	max++
+	id := strconv.Itoa(max)
+	customer = Customer{
+		Id:      id,
+		Name:    reqBody.Name,
 		Address: reqBody.Address,
 	}
 
-	return context.JSON(http.StatusOK, customersDB[id])
+	insertedResult, err := customerCollection.InsertOne(ctx, customer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Inserted %v\n", insertedResult.InsertedID)
+	return context.JSON(http.StatusOK, customer)
+}
+
+func getDB() (*mongo.Client, context.Context, *mongo.Collection) {
+	uri := os.Getenv("URI")
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	customerDatabase := client.Database("customerDB")
+	customerCollection := customerDatabase.Collection("customer")
+
+	return client, ctx, customerCollection
 }
 
 type Customer struct {
-	Id string
-	Name string
-	Address string
+	object_id primitive.ObjectID `bson:"_id,omitempty"`
+	Id        string             `json:"id,omitempty" bson:"id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	Address   string             `json:"address,omitempty" bson:"address,omitempty"`
 }
 
-var customersDB map[string]Customer
-var idCounter int
+type Customers struct {
+	customers []Customer
+}
 
 func main() {
-	// Initializing customer database with two entries
-	customersDB = map[string]Customer{
-		"1" : {
-			Id: "1",
-			Name: "Niloy Deb Roy",
-			Address: "Ja, 115/1, Mohakhali, Dhaka",
-		},
-		"2" : {
-			Id: "2",
-			Name: "Moeen Khan",
-			Address: "66, Mohakhali, Dhaka",
-		},
-	}
 
-	idCounter = len(customersDB) + 1
-
+	godotenv.Load()
 	// Getting port number from the envrionment variable
 	port := os.Getenv("CUSTOMERS_PORT")
 	if port == "" {
@@ -131,11 +210,11 @@ func main() {
 	e := echo.New()
 
 	e.Pre(middleware.RemoveTrailingSlash())
-	e.GET("/api/v1/customers", getAllCustomer)
-	e.GET("/api/v1/customers/:id", getCustomer, validateId)
-	e.PUT("/api/v1/customers/:id", updateCustomer, validateId)
-	e.DELETE("/api/v1/customers/:id", deleteCustomer, validateId)
-	e.POST("/api/v1/customers", addCustomer)
+	e.GET("/api/v1/customers", new(Customers).getAllCustomer)
+	e.GET("/api/v1/customers/:id", new(Customer).getCustomer, validateId)
+	e.PUT("/api/v1/customers/:id", new(Customer).updateCustomer, validateId)
+	e.DELETE("/api/v1/customers/:id", new(Customer).deleteCustomer, validateId)
+	e.POST("/api/v1/customers", new(Customer).addCustomer)
 
 	e.Logger.Print(fmt.Sprintf("Listening to the port: %s", port))
 	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
